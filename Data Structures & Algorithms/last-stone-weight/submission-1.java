d17e6f362b53218017432427edd96124990c68ce
Aldrin Saurov Sarker <A.saurov2016@gmail.com>
2026-09-23 12:23:29 +0600

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            queue.offer(stone);
+        }
+
+        while (queue.size() > 1) {
+            int x = queue.poll();
+            int y = queue.poll();
+
+            if (x != y) {
+                queue.offer(x - y);
+            }
+        }
+        return queue.isEmpty() ? 0 : queue.peek();
+    }
+}
