1533448e2ae975002024690cb99f0e09909cb9bd
Aldrin Saurov Sarker <A.saurov2016@gmail.com>
2026-09-23 12:39:07 +0600

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<double[]> queue = new PriorityQueue<>((a, b) -> Double.compare(b[1], a[1]));
+
+        for (int i = 0; i < points.length; i++) {
+            double distance = euclideanDistance(points[i]);
+            queue.offer(new double[]{i, distance});
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        
+        int[][] ans = new int[k][2];
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int index = (int) queue.poll()[0];
+            ans[i++] = points[index];
+        }
+        return ans;
+    }
+
+    public double euclideanDistance(int[] point) {
+        return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+    }
+}
